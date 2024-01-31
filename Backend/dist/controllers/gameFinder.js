f80c15cdda057a14d088e45e127be651a9e6978a
Tal <Tal>
2024-01-31 12:42:13 +0200

    gameFinder

--- Backend/dist/controllers/gameFinder.js
+++ Backend/dist/controllers/gameFinder.js
@@ -5,20 +5,48 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config({ path: '../../../.env' });
 const axios_1 = __importDefault(require("axios"));
-const games = 'Undertale⫻Outcore⫻Oneshot⫻Hat in Time⫻Star Apprentice⫻MC⫻Hollow Knight⫻Paper Mario⫻Stardew Valley⫻Henry Stickmin⫻Danganronpa⫻DDLC⫻Oxenfree⫻vividstasis⫻Stanley Parable⫻Cuphead⫻Celeste';
+const games = 'Undertale⫻Outcore: Desktop Adventure⫻OneShot: World Machine Edition⫻Hat in Time⫻Star Apprentice⫻Minecraft: Frozen⫻Hollow Knight⫻Paper Mario⫻Stardew Valley⫻Henry Stickmin⫻Danganronpa: Trigger Happy Havoc⫻Doki Doki Literature Club⫻Oxenfree⫻Vivid/Stasis⫻Stanley Parable⫻Cuphead⫻Celeste';
 const gamesArray = games.split('⫻');
+let gameWebsites = [];
+let gameArtworks = [];
+let gameCovers = [];
+let gameNames = [];
 //  console.log(process.env.ClientID, process.env.Authorization) //delete later
 //"fields url; where game=26226;"
-let data = 'fields name, artworks, cover.url, websites.url; search "Celeste"; limit 1;';
 for (let game in gamesArray) {
-    console.log(game);
+    let data = `fields name, artworks, artworks.url, cover.url, websites.url; search "${gamesArray[game]}"; limit 5;`;
+    console.log(game, gamesArray[game]);
+    setTimeout(() => { requestGame(data); }, 1000);
 }
-axios_1.default.post('https://api.igdb.com/v4/games', data, {
-    headers: {
-        'Client-ID': process.env.ClientID,
-        'Authorization': process.env.Authorization,
+// //for reusability
+function requestGame(data) {
+    axios_1.default.post('https://api.igdb.com/v4/games', data, {
+        headers: {
+            'Client-ID': process.env.ClientID,
+            'Authorization': process.env.Authorization,
+        }
+    }).then((response) => {
+        try {
+            // console.log("game", response.data[0].name, count); //count doesn't work well eitherway.
+            let gameWebsite = getFirstUrl(response.data[0].websites[0].url);
+            let gameArtwork = getFirstUrl(response.data[0].artworks[0].url);
+            let gameCover = getFirstUrl(response.data[0].cover.url);
+            let gameName = response.data[0].name;
+            console.log(gameWebsite, gameArtwork, gameCover, gameName); //delete later
+            gameNames.push(gameName);
+            gameWebsites.push(gameWebsite);
+            gameArtworks.push(gameArtwork);
+            gameCovers.push(gameCover);
+            console.log("Websites:", gameWebsites, "Artworks:", gameArtworks, "Covers:", gameCovers, "Names:", gameNames);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+}
+function getFirstUrl(field) {
+    if (field) {
+        console.log(field);
+        return field;
     }
-}).then((response) => {
-    console.log(response.data);
-    console.log("website", response.data.websites);
-});
+}
--- Backend/src/controllers/gameFinder.ts
+++ Backend/src/controllers/gameFinder.ts
@@ -1,23 +1,62 @@
 require('dotenv').config({ path: '../../../.env' });
 import axios from 'axios';
+import { request } from 'http';
 
-const games: string = 'Undertale⫻Outcore⫻Oneshot⫻Hat in Time⫻Star Apprentice⫻MC⫻Hollow Knight⫻Paper Mario⫻Stardew Valley⫻Henry Stickmin⫻Danganronpa⫻DDLC⫻Oxenfree⫻vividstasis⫻Stanley Parable⫻Cuphead⫻Celeste';
+const games: string = 'Undertale⫻Outcore: Desktop Adventure⫻OneShot: World Machine Edition⫻Hat in Time⫻Star Apprentice⫻Minecraft: Frozen⫻Hollow Knight⫻Paper Mario⫻Stardew Valley⫻Henry Stickmin⫻Danganronpa: Trigger Happy Havoc⫻Doki Doki Literature Club⫻Oxenfree⫻Vivid/Stasis⫻Stanley Parable⫻Cuphead⫻Celeste';
 const gamesArray: string[] = games.split('⫻');
+let gameWebsites: string[] = [];
+let gameArtworks: string[] = [];
+let gameCovers: string[] = [];
+let gameNames: string[] = [];
+
 //  console.log(process.env.ClientID, process.env.Authorization) //delete later
 //"fields url; where game=26226;"
-let data: string = 'fields name, artworks, cover.url, websites.url; search "Celeste"; limit 1;';
 
 for (let game in gamesArray) {
-    console.log(game)
+    let data: string = `fields name, artworks, artworks.url, cover.url, websites.url; search "${gamesArray[game]}"; limit 5;`;
+    console.log(game, gamesArray[game])
+    setTimeout(() => {requestGame(data)}, 1000);
 }
-axios.post('https://api.igdb.com/v4/games', data, {
+
+// //for reusability
+function requestGame(data: string):void {
+    axios.post('https://api.igdb.com/v4/games', data, {
     headers: {
         'Client-ID': process.env.ClientID,
         'Authorization': process.env.Authorization,
     }
-}).then((response: any) => {
-    console.log(response.data)
-    console.log("website", response.data.websites)
-})
+    }).then((response: any) => {
+        try {
+        // console.log("game", response.data[0].name, count); //count doesn't work well eitherway.
 
+        let gameWebsite = getFirstUrl(response.data[0].websites[0].url);
+        let gameArtwork = getFirstUrl(response.data[0].artworks[0].url);
+        let gameCover = getFirstUrl(response.data[0].cover.url);
+        let gameName = response.data[0].name;
+        //console.log(gameWebsite, gameArtwork, gameCover, gameName); //delete later
+        
+        gameNames.push(gameName);
+        gameWebsites.push(gameWebsite);
+        gameArtworks.push(gameArtwork);
+        gameCovers.push(gameCover);
 
+        console.log("Websites:",gameWebsites, "Artworks:",gameArtworks, "Covers:",gameCovers,"Names:",gameNames);
+
+        }
+        catch(err) {console.log(err)}
+    });
+}
+
+function getFirstUrl(field: any) {
+    if (field) {
+        console.log(field)
+        return field;
+    }
+}
+
+module.exports = {
+    gameNames,
+    gameWebsites,
+    gameArtworks,
+    gameCovers,
+}
