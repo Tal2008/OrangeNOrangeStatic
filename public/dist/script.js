14dafbe21f13373925ca4b030311d868a73b1f5d
Tal <Tal>
2024-02-04 10:52:38 +0200

    Images

--- public/dist/script.js
+++ public/dist/script.js
@@ -1,5 +1,6 @@
 "use strict";
-const games = 'Undertale⫻Outcore⫻Oneshot⫻Hat in Time⫻Star Apprentice⫻MC⫻Hollow Knight⫻Paper Mario⫻Stardew Valley⫻Henry Stickmin⫻Danganronpa⫻DDLC⫻Oxenfree⫻vividstasis⫻Stanley Parable⫻Cuphead⫻Celeste';
+const gamePics = document.getElementById('gamePics');
+const gamesString = 'Undertale⫻Outcore⫻Oneshot⫻Hat in Time⫻Star Apprentice⫻MC⫻Hollow Knight⫻Paper Mario⫻Stardew Valley⫻Henry Stickmin⫻Danganronpa⫻DDLC⫻Oxenfree⫻vividstasis⫻Stanley Parable⫻Cuphead⫻Celeste';
 window.addEventListener("DOMContentLoaded", () => {
     const placeholder = document.getElementById('image');
     console.log(placeholder);
@@ -9,12 +10,41 @@ window.addEventListener("DOMContentLoaded", () => {
     });
     //DOnt delete:
 });
-let gamesData = {};
-axios.get('./data/games').then((data) => {
-    gamesData = data;
+let dataLoaded = false;
+let gamesData;
+axios.get('./data/games').then((response) => {
+    gamesData = response.data;
+    console.log(response.data, gamesData); //delete later
+    dataLoaded = true;
 });
-console.log(gamesData);
-const gamesArray = games.split('⫻');
-console.log(gamesArray);
-for (let game in gamesArray) {
+if (dataLoaded) {
+    setTimeout(() => {
+        console.log(gamesData);
+    }, 200);
 }
+; //delete later}
+// Ok, so this might suck because the images are low-res. Welp, Guess it was a good learning experience.
+// Gonna probably do it manually or something.
+const gamesArray = gamesString.split('⫻');
+console.log(gamesArray);
+function createGame(gamesData) {
+    for (let game in gamesData.gameNames) {
+        let box = document.createElement('div');
+        box.classList.add('game');
+        gamePics === null || gamePics === void 0 ? void 0 : gamePics.appendChild(box);
+        let image = document.createElement('img');
+        image.classList.add('gamePictures');
+        box.appendChild(image);
+        image.src = gamesData.gameArtworks[game];
+        console.log(gamesData.Artworks); //deletre
+        let name = document.createElement('p');
+        box.appendChild(name);
+        name.innerHTML = gamesData.gameNames[game];
+        image.addEventListener('click', () => {
+            window.location.href = gamesData.gameWebsites[game];
+        });
+    }
+}
+setTimeout(() => {
+    createGame(gamesData);
+}, 200);
--- public/src/script.ts
+++ public/src/script.ts
@@ -1,6 +1,8 @@
 declare let axios: any;
 
-const games: string = 'Undertale⫻Outcore⫻Oneshot⫻Hat in Time⫻Star Apprentice⫻MC⫻Hollow Knight⫻Paper Mario⫻Stardew Valley⫻Henry Stickmin⫻Danganronpa⫻DDLC⫻Oxenfree⫻vividstasis⫻Stanley Parable⫻Cuphead⫻Celeste';
+const gamePics = document.getElementById('gamePics');
+
+const gamesString: string = 'Undertale⫻Outcore⫻Oneshot⫻Hat in Time⫻Star Apprentice⫻MC⫻Hollow Knight⫻Paper Mario⫻Stardew Valley⫻Henry Stickmin⫻Danganronpa⫻DDLC⫻Oxenfree⫻vividstasis⫻Stanley Parable⫻Cuphead⫻Celeste';
 
 window.addEventListener("DOMContentLoaded", () => {
     const placeholder: HTMLImageElement | null = document.getElementById('image') as HTMLImageElement;
@@ -16,15 +18,46 @@ window.addEventListener("DOMContentLoaded", () => {
 
 });
 
-let gamesData = {};
-axios.get('./data/games').then((data: any) => {
-    gamesData = data;
+let dataLoaded: boolean = false;
+let gamesData: object;
+axios.get('./data/games').then((response: any) => {
+    gamesData = response.data;
+    console.log(response.data, gamesData) //delete later
+    dataLoaded = true;
 });
-console.log(gamesData);
+if (dataLoaded) {setTimeout(() => {
+    console.log(gamesData)
+}, 200);}; //delete later}
 
-const gamesArray: string[] = games.split('⫻');
+
+// Ok, so this might suck because the images are low-res. Welp, Guess it was a good learning experience.
+// Gonna probably do it manually or something.
+
+const gamesArray: string[] = gamesString.split('⫻');
 console.log(gamesArray)
 
-for (let game in gamesArray) {
+function createGame(gamesData: any): void {
+    for (let game in gamesData.gameNames) {
+        let box: HTMLDivElement = document.createElement('div');
+        box.classList.add('game');
+        gamePics?.appendChild(box);
 
+        let image: HTMLImageElement = document.createElement('img');
+        image.classList.add('gamePictures');
+        box.appendChild(image);
+        image.src = gamesData.gameArtworks[game];
+        console.log(gamesData.Artworks) //deletre
+
+        let name = document.createElement('p');
+        box.appendChild(name)
+        name.innerHTML = gamesData.gameNames[game];
+
+        image.addEventListener('click', () => {
+            window.location.href=gamesData.gameWebsites[game];
+        });
+    }
 }
+
+setTimeout(() => {
+    createGame(gamesData);
+}, 200);
