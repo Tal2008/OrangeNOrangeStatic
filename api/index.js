50603f48259a15fb235441c310ceb72051a1025a
Tal <Tal>
2024-03-10 16:31:17 +0200

    vercel test

---
+++ api/index.js
@@ -0,0 +1,17 @@
+"use strict";
+require('dotenv').config({ path: '../../.env' });
+const path = require('path');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const gameGiver = require('./Routes/gameGiver');
+const ostGiver = require('./Routes/ostGiver');
+const PORT = 8080;
+app.use('/', express.static(path.join(__dirname, '..', '..', 'public')));
+console.log(path.join(__dirname, '..', '..', 'public'));
+app.use('/data/games', gameGiver);
+app.use('/data/ost', ostGiver);
+//move this to another file later
+//app.listen(PORT);
+//module.exports = app;
+
