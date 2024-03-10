c37e8401fe835cd33065d4897d701d92e8abb1d7
Tal <Tal>
2024-03-10 16:20:08 +0200

    pain

--- api/server.js
+++ api/server.js
@@ -1,14 +1,2 @@
-const express = require('express');
 const app = require('../Backend/dist/app');
-const { createServer } = require('http');
-const { parse } = require('url');
-const { Server } = require('http');
-
-module.exports = (req, res) => {
-  const parsedUrl = parse(req.url, true);
-  const { pathname, query } = parsedUrl;
-
-  const server = new Server(app);
-  server.on('request', app);
-  server.emit('request', req, res);
-};
+module.exports = app;
